2f15cfd30bd3f6f9ea0a0c02748722add50e6ef7
Yuliya Mylnikova <jucharia87@gmail.com>
2023-03-07 23:51:50 +0300

    Add CsvFile

--- src/test/java/RestWorkServiceTest.java
+++ src/test/java/RestWorkServiceTest.java
@@ -1,16 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqa.javaqamvn.services.RestWorkService;
 
 public class RestWorkServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "3, 90000, 30000, 140000",
-            "3, 10000, 3000, 20000",
-            "2, 100000, 60000, 150000",
-    })
+  //  @CsvSource({
+   //         "3, 90000, 30000, 140000",
+    //        "3, 10000, 3000, 20000",
+   //         "2, 100000, 60000, 150000",
+  //  })
+    @CsvFileSource(files = "src/test/resources/months.csv")
     public void shouldCalcMonth1(int expected, int income, int expenses, int threshold) {
         RestWorkService service = new RestWorkService();
 
